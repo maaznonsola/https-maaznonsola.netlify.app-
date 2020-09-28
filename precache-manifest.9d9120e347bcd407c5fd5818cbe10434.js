ffc632a37226ccb39a39307897f0080d71d74aca
Maaz Nonsola <58545161+maaznonsola@users.noreply.github.com>
2020-09-28 13:08:28 +0530

    Add files via upload

---
+++ precache-manifest.9d9120e347bcd407c5fd5818cbe10434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b80c79bbbb95ee2d5747aab9fb67af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c5066d651a21c4d0fe",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c5066d651a21c4d0fe",
+    "url": "/static/js/main.ddbc7e8b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
